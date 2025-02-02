aa1017375df6aa9d2efb090e66960f9370d7f7fa
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-02-02 17:29:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://colorful-deeann-aryabots-2749e675.koyeb.app/")
+URL = environ.get("URL", "https://greasy-bathsheba-aryahouse-2ffcc966.koyeb.app/")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002091553156'))
